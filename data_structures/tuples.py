60be1a537ccde17ba67102d3002f25814d22be90
Toludoyin <toludoyinshopein@gmail.com>
2022-07-23 11:05:57 +0100

    added tuples

--- data_structures/tuples.py
+++ data_structures/tuples.py
@@ -1,2 +1,9 @@
 
-# tuples are immutable
+# tuples are immutable, can not be manipulated
+empty_tuple = ()
+numbers = (2,4,6,8,9,1)
+print(type(numbers))
+
+print(numbers[3]) # access a tuple
+del numbers
+print(numbers)  # this will give an error cause the variable no longer exist
