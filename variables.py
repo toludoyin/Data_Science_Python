ab7b68299b6da1374ef14f8c88fb21275f3cff24
Toludoyin <toludoyinshopein@gmail.com>
2022-07-30 07:30:28 +0100

    added c-style string formating

--- variables.py
+++ variables.py
@@ -1,6 +1,5 @@
 
-# datatypes
-strings_ = 'Gender'
+strings_ = 'Gender'   # datatypes
 float_ = 3.14
 integers_ = 654
 booleans_ = True or False
@@ -13,10 +12,12 @@ list_ = ['name', 35, True, 13.67]
 tuple_ = ('name', 35, True, 13.67)
 print(f'{type(set_)}\n')
 
+print('String formating in text %s numbers %d and float %f' % (strings_,  integers_, float_))          # c-style string formatting
 
 
 
-# operation evaluation rule; parenthesis, exponential, (multiplication division, remainder), (addition, subtraction) -->left >> right.
+
+# basic operation evaluation rule; parenthesis, exponential, (multiplication division, remainder), (addition, subtraction) -->left >> right.
 number = 13 + 8 * 9 / 4 ** 3
 print(f'Expression result: {round(number,2)}\n')
 
@@ -48,7 +49,6 @@ intro = input('what is your name? ').title()
 intro1 = input(str("Let's get to know you, what is your favorite programming language?")).upper()
 result = f"Welcome {intro}. We love your,favorite programming,language {intro1}\n"
 print(result.replace(',', ' '))  # string manipulation
-
 
 
 # an input function to give users their toatal pay based on their imput
