e34638c97fc425502c28ed69481d9152bd98bc06
Toludoyin <toludoyinshopein@gmail.com>
2022-07-30 09:00:14 +0100

    added computer guess

--- iterators/while_loop.py
+++ iterators/while_loop.py
@@ -53,3 +53,24 @@ guess(10)
 
 
 
+# def computer_guess(x):
+#     low = 1
+#     high = x
+#     feedback = " "
+
+#     while feedback != "C":
+#         if low != high:
+#             guess = random.randint(low,high)
+#         else:
+#             guess = low
+#             feedback = input(f"Is {guess} too high(H), too low(L) or correct(C):").lower()
+#         if feedback == "h":
+#                 high = guess -1
+#         elif feedback == "l":
+#                 low = guess +1
+#     print(f"Yoo.., the computer guessed the number{guess},correctly!.")
+
+# computer_guess(10)
+
+
+
