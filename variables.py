d99baa22ca52b9dbf542a62422453ca6a24c8162
Toludoyin <toludoyinshopein@gmail.com>
2022-09-15 06:06:57 +0100

    add-strings-library

--- variables.py
+++ variables.py
@@ -15,8 +15,6 @@ print(f'{type(set_)}\n')
 print('String formating in text %s numbers %d and float %f' % (strings_,  integers_, float_))          # c-style string formatting
 
 
-
-
 # basic operation evaluation rule; parenthesis, exponential, (multiplication division, remainder), (addition, subtraction) -->left >> right.
 number = 13 + 8 * 9 / 4 ** 3
 print(f'Expression result: {round(number,2)}\n')
@@ -29,9 +27,6 @@ c = 12
 quadratic_equation = -b +- (math.sqrt(b ** 2) - (4 * a * c))/ 2 * a  #expression
 print(f'The quadratic equation result is %s \n' % quadratic_equation)
 
-
-
-
 # swapping variables
 adapter = 'postilion'
 intent = 'reversal'
@@ -41,17 +36,24 @@ adapter = intent
 intent = bank
 print(f'{intent} \n')
 
-
-
-
 # input finction to welcome user
 intro = input('what is your name? ').title()
 intro1 = input(str("Let's get to know you, what is your favorite programming language?")).upper()
 result = f"Welcome {intro}. We love your,favorite programming,language {intro1}\n"
 print(result.replace(',', ' '))  # string manipulation
 
-
 # an input function to give users their toatal pay based on their imput
 hrs = input('Enter Hours to learn: ')
 rate = input('Enter Rate: ')
 print('Pay:', float(hrs) * float(rate))
+
+# strings library
+name = ' TOLUDOYIN SHOPEIN '  # stripping whitespace
+name.rstrip()
+name.lstrip()
+name.strip()
+name.startswith(' TOLU')
+
+
+
+
