673d038d05df94a344bd5e317d777d77cf82bcc4
Toludoyin <toludoyinshopein@gmail.com>
2022-10-16 22:02:32 +0100

    modify-file

--- study_test/python_library.py
+++ study_test/python_library.py
@@ -1,12 +1,10 @@
 # QUESTION
 # Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. Convert the extracted value to a floating point number and print it out.
-
 text = "X-DSPAM-Confidence:    0.8475"
 new_text = text.find(":")
 float_value = text[new_text+1:]
 clean_float = float_value.strip()
 print(float(clean_float))
-
 
 # Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
 # X-DSPAM-Confidence:    0.8475
@@ -35,3 +33,19 @@ try:
 except ZeroDivisionError:
     avg =0
 print("Average spam confidence:", avg)
+
+fruit = ['orange mango pineapple watermelon']
+fruits =fruit.split()
+print(fruits)
+
+# 8.4 Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in python sort() order as shown in the desired output. You can download the sample data at http://www.py4e.com/code3/romeo.txt
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    word_split = line.split()
+    for word in word_split:
+        if word not in lst:
+            lst.append(word)
+        else: continue
+print(sorted(lst))
