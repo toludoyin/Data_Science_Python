da9861e9d289c04269b95f3f2ebc19485dd6dd56
Toludoyin <toludoyinshopein@gmail.com>
2022-10-18 10:28:56 +0100

    loop-thrg-list

--- study_test/data_structures/list.py
+++ study_test/data_structures/list.py
@@ -1,12 +1,12 @@
 #  list
 courses = []
-courses.extend([499, [1.2, 45.6], 1000, 412,"pass", "secondclass", True]) # append multiple element
+courses.extend([499, [17,45.6], 10.00, "pass", "secondclass", True]) # append multiple element
 courses.append("firstclass")
 course = [1,2,3,4]
 print('Using operators to append multiple list: %s + %s' % (courses, course))  # list can contain any set of datatype
 
 courses1 = courses[5]
-print(courses1[1]) # access list in list
+print(courses1) # access list in list
 courses.pop(-3)  # delete number in a list
 print(courses)
 print(courses[0:3])  # slicing
@@ -23,7 +23,10 @@ print(learning.index("datacamp"))
 learning[0], learning[-1] = learning[-1], learning[0]
 print(learning)
 
-first_draft = [3,10,4,7,5,6]
+first_draft = [3,10,4,7]  # use for-loop to append a list
+second=[1.0,4,7,1]
+for ss in second:
+    first_draft.append(ss)
 first_draft.sort()   # sort list
 print(first_draft)
 
