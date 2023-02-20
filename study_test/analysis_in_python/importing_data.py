2c24cb127ac1fe2736b7f5920a26b2a4c609fc90
Toludoyin <toludoyinshopein@gmail.com>
2023-02-20 11:55:53 +0100

    remove excel import method

--- study_test/analysis_in_python/importing_data.py
+++ study_test/analysis_in_python/importing_data.py
@@ -1,10 +1,13 @@
 #  importing a text file
-with open(file_name, 'r') as file:
+with open('file_name', 'r') as file:
     print(file.read())
 
 # importing csv
 import pandas as pd
-df = pd.read_csv(file_name)
+df = pd.read_csv('file_name')
 df.head()
 
-# 
+# importing pickled file
+with open('file_name.pkl', 'rb') as file:
+    d = file.load(file)
+print(d)
