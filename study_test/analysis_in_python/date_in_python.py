e713e10276192f496c941724da28f02a1bc23f96
Toludoyin <toludoyinshopein@gmail.com>
2023-03-04 23:30:19 +0100

    add date-to-study

---
+++ study_test/analysis_in_python/date_in_python.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+date_sample = date(2023, 3, 4)
+
+print(date_sample.day)
+print(date_sample.month)
+print(date_sample.year)
+print(date_sample.weekday())
+
+# math with date
