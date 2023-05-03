0ddb9982f97b7702101b17ff1fe031b4134e826f
Toludoyin <toludoyinshopein@gmail.com>
2023-05-03 02:04:01 +0100

    merge dict

--- study_test/data_structures/dictionaries.py
+++ study_test/data_structures/dictionaries.py
@@ -13,6 +13,17 @@ values = [3, 10, 4, 7]
 result = dict(zip(key, values))
 print(result)
 
+# merge 2 dictionaries
+def merge(dict1, dict2):
+    answer = dict1 .copy()
+    answer.update(dict2)
+    return answer
+
+dict1 = {'name': 'jam jam',  'age': 100}
+dict2 = {'married': False, 'job': 'data scientist'}
+merge(dict1, dict2)
+
+
 # 9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
 name = input("Enter file:")
 if len(name) < 1:
